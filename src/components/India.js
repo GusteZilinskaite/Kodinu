bcc78f10a7a8d213fde88b7eaacaf4b2bd4302ac
GusteZilinskaite <64253268+GusteZilinskaite@users.noreply.github.com>
2020-04-24 11:45:34 +0300

    Add files via upload

---
+++ src/components/India.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import  '../StyleIndia.css';
+
+const India = () => {
+    return (<>
+        <div className='main-india'>
+            <div className='pic-list'>
+                <ul>
+                <li><div className='india-img img1india'></div></li>
+                <li><div className='india-img img2india'></div></li>
+                <li><div className='india-img img3india'></div></li>
+                <li></li>
+                </ul>
+            </div>
+            <div className='big-pic'></div>
+        </div>
+
+
+    </>
+        
+    )
+}
+
+export default India;
+
